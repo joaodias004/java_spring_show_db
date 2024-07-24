aa3b24e846375c56445dec71b80fdbc1a5233a91
joaodias004 <jdrdl11@gmail.com>
2024-07-24 10:39:03 -0300

    Version 1.0
    Finishes the initial concept.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.model.DadosSerie;
-import com.example.demo.service.ConverteDados;
+import com.example.demo.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.demo.service.ConsumoAPI;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -16,11 +14,7 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=d4bf6f4e");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
